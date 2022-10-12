668156431cd01ed9400f2cce2439dd5efdc64829
Mario Lopez <mario.lopez.marquez@uabc.edu.mx>
2022-10-11 23:18:00 -0700

    se creo zen372.py y main.py

---
+++ zen372.py
@@ -0,0 +1,17 @@
+import this
+#comentario 1 linea
+"""
+comentario multilinea
+1 linea
+2 lineas
+"""
+
+print ("latosa1: nose")
+print ("latosa2: mas vale pedir perdon que pedir permiso")
+print ("Marco1: camaron que se duerme bla bla bla")
+print ("Marco2: fuga!!")
+print ("derek: fuga x2!!")
+
+barba="Algo que sorpresa!!"
+
+
